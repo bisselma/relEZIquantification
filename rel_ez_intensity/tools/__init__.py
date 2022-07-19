5c6f94b063d79769a56cb15f4c040f1a3a7f1f51
Ben Isselmann <ben.isselmann@gmail.com>
2022-07-19 16:17:54 +0200

    error import

--- rel_ez_intensity/tools/__init__.py
+++ rel_ez_intensity/tools/__init__.py
@@ -4,7 +4,3 @@ __author__ = """Ben Isselmann"""
 __email__ = "ben.isselmann@gmail.com"
 __version__ = "dev0.0.0"
 
-
-from rel_ez_intensity import (
-    base
-)
--- rel_ez_intensity/tools/utils.py
+++ rel_ez_intensity/tools/utils.py
@@ -11,7 +11,8 @@ from scipy.ndimage import shift
 from scipy.ndimage.morphology import binary_dilation
 import eyepy as ep
 
-import rel_ez_intensity.base as b
+import rel_ez_intensity.base
+
 
 def get_id_by_file_path(
     file_path: Optional[str] = None,
@@ -63,7 +64,7 @@ def get_rpedc_list(
 def get_rpedc_map(
     file_path: Union[str, Path, IO] = None,
     scan_size: Optional[tuple] = None,
-    mean_rpedc: Optional[b.OCTMap] = None,
+    mean_rpedc: Optional[np.ndarray] = None,#: Optional[OCTMap] = None,
     laterality: Optional[str] = None,
     translation: Optional[tuple] = None
     ) -> np.ndarray:
