69adf5282f9ce7e0a8969e451db046a4c1bf2acc
Ben Isselmann <ben.isselmann@gmail.com>
2022-10-05 12:47:40 +0200

    exclude git repo in requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 __author__ = """Ben Isselmann"""
 __email__ = "ben.isselmann@gmail.com"
-__version__ = "0.3.7"
+__version__ = "0.3.8"
 
 
 #with open("README.rst") as readme_file:
@@ -17,8 +17,10 @@ __version__ = "0.3.7"
 
 requirements = ["eyepie==0.3.7", "numpy>=1.22", "opencv-python-4.5.3.56", "pillow==9.0.1",
     "xlsxwriter", "read-roi", "imgaug==0.4.0", "matplotlib==3.6", "PyYAML==6.0", "scikit-image", "scikit_learn==1.1.1", "scipy==1.8.0",
-    "torch==1.8.1", "torchvision==0.9.1", "tqdm==4.64.0", "http://gitlab.grade-rc.de/rc-weinz/heyex_tools.git",
-    "git+http://gitlab.grade-rc.de/rc-weinz/macustarpredictor.git", "pandas", "ipywidgets"]
+    "torch==1.8.1", "torchvision==0.9.1", "tqdm==4.64.0", "pandas", "ipywidgets"]
+
+    #"http://gitlab.grade-rc.de/rc-weinz/heyex_tools.git",
+    #"git+http://gitlab.grade-rc.de/rc-weinz/macustarpredictor.git"
 
 setup_requirements = ["pytest-runner"]
 
