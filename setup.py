e0a2b209caacd334a6c6ece6a9a45935b0c2ef15
Ben Isselmann <ben.isselmann@gmail.com>
2022-11-04 15:48:23 +0100

    if stimuli is noch given change return masl dafault value to -1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 __author__ = """Ben Isselmann"""
 __email__ = "ben.isselmann@gmail.com"
-__version__ = "0.0.32"
+__version__ = "0.0.33"
 
 
 #with open("README.rst") as readme_file:
