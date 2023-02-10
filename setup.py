5547357d4dfd7ea22c1906fbc2514b47d0f05d2f
Ben Isselmann <ben.isselmann@gmail.com>
2023-02-10 13:55:44 +0100

    minor changes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 __author__ = """Ben Isselmann"""
 __email__ = "ben.isselmann@gmail.com"
-__version__ = "0.0.10118"
+__version__ = "0.0.10119"
 
 
 #with open("README.rst") as readme_file:
